101b2b88a7f571723d2c8f265e854031009f48b1
Matthew Tang <jmtang10@gmail.com>
2014-11-05 20:41:08 -0600

    Add Test
    Added test files.

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,36 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var name = "Matthew"
+var title = "Mr."
+var returnValue = "Hello \(title) \(name)"
+var strongInt:Int = 1
+var score = 125
+var multiplier = 4
+var output = "Your score is: \(score * multiplier)"
+let player = "Matt"
+"\(player) lalala"
+
+var x = 0
+while (x<5){
+    println(x)
+    x++
+}
+
+if (x == 5){
+    println("yup")
+}
+
+func getX() -> (Int, Int, Int) {
+    return (1, 2, 3)
+}
+
+var y = getX()
+
+y.0
+
+
+
+
